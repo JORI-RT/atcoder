42f80e75d3516fba190f16c514e01bef8e4c0c45
ryo <sigaryoutakaki@gmail.com>
2020-04-30 21:19:30 +0900

    refact

--- aoj/data_structure/stack.py
+++ aoj/data_structure/stack.py
@@ -3,19 +3,38 @@
 a = list(map(str, input().split()))
 
 
+class MyStack:
+    def __init__(self, data=[]):
+        self.data = data
+
+    def push(self, x):
+        self.data.append(x)
+
+    def pop(self):
+        if len(self.data) == 0:
+            return "Stack is Empty"
+        return self.data.pop()
+
+
 def is_op(s):
     if s == '+' or s == '-' or s == '*':
         return True
     return False
 
 
-a2 = []
-for s in a:
-    if is_op(s):
-        n2 = a2.pop()
-        n1 = a2.pop()
-        a2.append(eval(str(n1) + s + str(n2)))
-    else:
-        a2.append(int(s))
+def m(a):
+    a2 = MyStack()
+    for s in a:
+        if is_op(s):
+            n2 = a2.pop()
+            n1 = a2.pop()
+            a2.push(eval(str(n1) + s + str(n2)))
+        else:
+            a2.push(int(s))
 
-print(a2.pop())
+    return a2
+
+
+a3 = m(a)
+
+print(a3.pop())
