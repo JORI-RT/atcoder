6b1ecfb7e1a646fa379ac18efe9658ba1db89ac5
ryo <sigaryoutakaki@gmail.com>
2020-05-06 20:28:31 +0900

    addddd

---
+++ mizuiro/11.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+K = []
+S = []
+for m in range(M):
+    inp = list(map(int, input().split()))
+    K.append(inp[0])
+    S.append(inp[1:])
+
+P = list(map(int, input().split()))
+
+
